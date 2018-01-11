fe86d5291c4c2a5cbfeec397a488bb2a969d7b12
C-RICHMOND <christopherrichmond@outlook.com>
2018-01-11 03:51:54 -0500

    Index File

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<style>
+	body {
+		background-color: #E9E9E9;
+	}
+	
+	.navbar {
+		background-color: #CDCDCD;
+	}
+	
+	.navbar .header {
+		text-align: center;
+	}
+</style>
+<body>
+<nav class="navbar navbar-default">
+	<div class="navbar header">
+		<h1>To Do Application</h1>
+	</div>
+</nav>
+<div class="container-fluid">
+	<div style="text-align: center";>
+		<button type="button" class="btn btn-primary">Create Task</button>
+	</div>
+</div>
+</body>
+</html>
+
+
+<?php ?>
